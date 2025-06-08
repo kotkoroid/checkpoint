f4c0c6aebf2a457bab9f48b98ef3df72397ee4ee
Kotkoroid <git@kotkoroid.com>
2025-06-08 14:01:15 +0200

    chore: update vitest to 3.2 (#47)

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,16 +2,19 @@ import { coverageConfigDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
-		exclude: ['**/tsbuild'],
-		workspace: ['gateway/vitest.config.ts', 'services/**/vitest.config.ts'],
+		projects: [
+			'./gateway/vitest.config.ts',
+			'./services/identity/vitest.config.ts',
+			'./services/passport/vitest.config.ts',
+		],
 		coverage: {
+			// TODO: Using V8 for code coverage is not yet supported: https://developers.cloudflare.com/workers/testing/vitest-integration/known-issues/#coverage
 			provider: 'istanbul',
 			exclude: [
 				...coverageConfigDefaults.exclude,
+				'./*.config.ts',
 				'**/tsbuild',
-				'sst.config.ts',
 			],
-			excludeAfterRemap: true,
 		},
 	},
 });

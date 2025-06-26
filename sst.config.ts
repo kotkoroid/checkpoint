e8cfdfd5ab405d84b8fef39425cf1e803670839b
Kotkoroid <git@kotkoroid.com>
2025-06-26 08:09:36 +0200

    chore: rename repository (#69)

--- sst.config.ts
+++ sst.config.ts
@@ -1,21 +1,20 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-
 export default $config({
 	app(input) {
 		return {
-			name: 'minerva',
-			removal: input?.stage === 'production' ? 'retain' : 'remove',
-			protect: ['production'].includes(input?.stage),
+			name: 'checkpoint',
+			removal: input.stage === 'production' ? 'retain' : 'remove',
+			protect: ['production'].includes(input.stage),
 			home: 'cloudflare',
 			providers: { cloudflare: '6.2.0' },
 		};
 	},
 	async run() {
-		// Identity D1
+		// Identity
 		new sst.cloudflare.D1('identityD1', {
 			transform: {
 				database: {
-					name: 'checkpoint-identity',
+					name: `checkpoint-identity-${$app.stage}`,
 				},
 			},
 		});

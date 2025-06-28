b39345c549403d6d74916b8c67a5597d668cac48
kotkoroid <git@kotkoroid.com>
2025-06-28 18:10:01 +0200

    chore: remove unused code

--- gateway/src/index.ts
+++ gateway/src/index.ts
@@ -1,13 +1,10 @@
 import observability from '@checkpoint/gateway/src/api/observability';
-import tokens from '@checkpoint/gateway/src/api/tokens';
 import users from '@checkpoint/gateway/src/api/users';
 import { OpenAPIHono } from '@hono/zod-openapi';
 
 const app = new OpenAPIHono<{ Bindings: GatewayEnv }>();
 
 app.route('/', observability);
-
-app.route('/', tokens);
 
 app.route('/', users);
 

9d4d3cdf1a27f7b64870eb5c7ac8772eec4cda7a
Kotkoroid <git@kotkoroid.com>
2025-07-18 20:38:32 +0200

    chore: remove vite (#97)

--- services/identity/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { cloudflare } from '@cloudflare/vite-plugin';
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-	server: {
-		port: 50000,
-	},
-	plugins: [cloudflare()],
-});
